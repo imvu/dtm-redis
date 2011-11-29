9e6edeaf4d60f5bdc3261ac8942d9202866726c7
Eric Hohenstein <ehohenstein@imvu.com>
2011-11-29 13:41:19 -0800

    added unit tests for txn monitor event handlers and fixed some bugs

--- txn_monitor.erl
+++ txn_monitor.erl
@@ -2,6 +2,8 @@
 -export([start/0, allocate/1, finalized/1]).
 -compile(export_all).
 
+-include_lib("eunit/include/eunit.hrl").
+
 -record(allocate, {from, buckets}).
 -record(txn_id, {monitor, id}).
 -record(finalized, {id, bucket}).
@@ -15,25 +17,41 @@ start() ->
 loop(State) ->
     receive
         #allocate{from=From}=Allocate ->
-            {Id, Transactions} = handle_allocate(Allocate, State),
+            {Id, NewState} = handle_allocate(Allocate, State),
             From ! {self(), #txn_id{monitor=self(), id=Id}},
-            loop(State#state{next_id=Id+1, transactions=Transactions});
+            loop(NewState);
         #finalized{}=Finalize ->
-            loop(handle_finalize(Finalize, State));
+            NewState = handle_finalize(Finalize, State),
+            loop(NewState);
         Any ->
             io:format("transaction monitor received message ~p~n", [Any]),
             loop(State)
     end.
 
 handle_allocate(#allocate{buckets=Buckets}, #state{}=State) ->
-    % TODO: Verify that Buckets is a non-empty set
+    true = sets:is_set(Buckets),
     Id = State#state.next_id,
-    {Id, dict:store(Id, Buckets, State#state.transactions)}.
+    {Id, State#state{next_id=Id+1, transactions=update_transaction(Id, Buckets, State#state.transactions)}}.
 
-handle_finalize(#finalized{id=Id, bucket=Bucket}, State) ->
-    Buckets = sets:erase(Bucket, dict:fetch(Id, State#state.transactions)),
-    % TODO: Check for an empty buckets set and dispose of the transaction when empty
-    State#state{transactions=dict:store(Id, Buckets)}.
+handle_allocate_test() ->
+    Transactions = dict:store(42, sets:add_element(1234, sets:new()), dict:new()),
+    {42, #state{next_id=43, transactions=Transactions}} = handle_allocate(#allocate{buckets=sets:add_element(1234, sets:new())}, #state{next_id=42, transactions=dict:new()}).
+
+handle_finalize(#finalized{id=Id, bucket=Bucket}, #state{}=State) ->
+    Buckets = sets:del_element(Bucket, dict:fetch(Id, State#state.transactions)),
+    State#state{transactions=update_transaction(Id, Buckets, State#state.transactions)}.
+
+handle_finalize_test() ->
+    Result = #state{transactions=dict:store(42, sets:add_element(1234, sets:new()), dict:new())},
+    Result = handle_finalize(#finalized{id=42, bucket=4321}, #state{transactions=dict:store(42, sets:add_element(1234, sets:add_element(4321, sets:new())), dict:new())}).
+
+update_transaction(Id, Buckets, Transactions) ->
+    case sets:size(Buckets) of
+        % TODO: dispose of the transaction from disk
+        0 ->
+            dict:erase(Id, Transactions);
+        _Any -> dict:store(Id, Buckets, Transactions)
+    end.
 
 allocate(Monitors) ->
     Monitor = lists:nth(random:uniform(length(Monitors)), Monitors),
