0ea3abc2072d78964872cb3ba548275348cc0a17
Eric Hohenstein <ehohenstein@imvu.com>
2012-02-05 17:24:44 -0800

    placing dtm-redis under MIT license

--- master.sh
+++ master.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 
 erl -pa lib/eredis/ebin/ lib/erlymock/ebin/ boot start_clean -kernel inet_dist_listen_min 50000 inet_dist_listen_max 50009 -name redis -setcookie dtmredis -s dtm_redis server_start -extra config/cluster
 
